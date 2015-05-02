211addc1acafc2f079005972078ed131cadf3c7b
Duncan Malashock <duncan@emissary.io>
2015-05-02 19:13:47 -0400

    Change to spec syntax

--- test/test_point.rb
+++ test/test_point.rb
@@ -1,12 +1,12 @@
 require "minitest/autorun"
 require "ivan/primitives/point"
 
-class TestPoint < Minitest::Test
-  def setup
+describe Point do
+  before do
     @point = Point.new([0,0,0])
   end
 
-  def test_that_it_has_a_coordinate
-    assert_equal [0,0,0], @point.coordinate
+  it "must have a coordinate" do
+    @point.coordinate.must_equal [0,0]
   end
 end
