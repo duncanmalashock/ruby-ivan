a186cd135bf681a00975b4f84e44f8034f4d479f
Duncan Malashock <duncan@emissary.io>
2015-05-03 23:02:57 -0400

    Remove requires on unused files

--- lib/ivan.rb
+++ lib/ivan.rb
@@ -3,16 +3,13 @@ require_relative 'ivan/exceptions'
 
 require 'serialport'
 
-require_relative 'ivan/compositor'
-require_relative 'ivan/usb_teensy_renderer'
-require_relative 'ivan/stub_renderer'
+require_relative 'ivan/view'
+require_relative 'ivan/renderer'
 
 require_relative 'ivan/primitives/point'
 require_relative 'ivan/primitives/line'
-
-require_relative 'ivan/spatial'
-require_relative 'ivan/scene_objects/scene_object'
-require_relative 'ivan/scene_objects/cube'
+require_relative 'ivan/actors/actor'
+require_relative 'ivan/actors/cube'
 
 module Ivan
 end
