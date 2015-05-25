7e708b77697453175b66c24d91996ef84c5cf852
Duncan Malashock <duncan@emissary.io>
2015-05-24 22:47:35 -0400

    Preparatory methods are called for both lines and points

--- lib/ivan/renderer.rb
+++ lib/ivan/renderer.rb
@@ -27,6 +27,38 @@ class Renderer
 
   private
 
+    def scale_points(points)
+      scale_factor = 128.0
+      points.each do |p|
+        p[0] = p[0] * scale_factor + 128.0
+        p[1] = p[1] * scale_factor + 128.0
+        p[2] = p[2] * scale_factor + 128.0
+      end
+      return points
+    end
+
+    def clip_lines(lines)
+      lines.each do |l|
+        if l[0][0] < 0
+          lines.delete(l)
+        end
+      end
+      return lines
+    end
+
+    def clip(in_val)
+      if (in_val.infinite?)
+        in_val = 0
+      end
+      out_val = in_val.floor
+      if out_val <= 0
+        return 0
+      elsif out_val >= 255
+        return 255
+      end
+      return out_val
+    end
+
     def transmit_instruction_length(length)
       write_16_bit_word(length)
     end
