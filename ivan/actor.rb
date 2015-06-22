46b87950b5613e24587d2cb36a08e230bbc530f8
Duncan Malashock <duncan@emissary.io>
2015-06-22 02:09:51 -0400

    Actor class now takes arbitrary geometry struct, so it can be more flexible at runtime

--- ivan/actor.rb
+++ ivan/actor.rb
@@ -1,8 +1,21 @@
 class Actor
-  attr_reader :points
-  attr_reader :lines
-  def initialize(points, lines)
-    @points = points
-    @lines = lines
+  include Spatial
+
+  def initialize(geometry)
+    @points = geometry[:points].map do |p|
+      Point.new(*p)
+    end
+    @lines = geometry[:lines]
   end
+
+  def instructions
+    my_instructions = []
+    @lines.each do |l|
+      l.each do |p|
+        my_instructions << @points[p]
+      end
+    end
+    my_instructions
+  end
+
 end  
