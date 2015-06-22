9a847a6bc991dd681acf7b395b2ad80757ff7012
Duncan Malashock <duncan@emissary.io>
2015-06-22 13:01:22 -0400

    Test concrete TeensyVSender instead of abstract Sender class

--- test/test_teensyv_sender.rb
+++ test/test_teensyv_sender.rb
@@ -1,23 +1,17 @@
 require_relative "minitest_helper"
 
-describe Sender do
+describe TeensyVSender do
   before do
     @config_params = {
-      param1: "default"
+      output_class: TeensyVOutputDouble
     }
-    @sender = Sender.new(@config_params)
+    @sender = TeensyVSender.new(@config_params)
   end
 
   describe "when initialized correctly" do
     it "sends post_initialize to itself" do
-      assert_kind_of Sender, @sender
+      assert_kind_of TeensyVSender, @sender
       assert_send [@sender, :post_initialize, @config_params]
-    end
-    it "responds to output_message" do
-      assert_respond_to @sender, :output_message
-    end
-    it "responds to coordinate_format" do
-      assert_respond_to @sender, :coordinate_format
     end
   end
 
@@ -25,8 +19,6 @@ describe Sender do
     it "sends pre_send_buffer to itself" do
       @sender.send_buffer(BufferFixture.instructions)
       assert_send [@sender, :pre_send_buffer, BufferFixture.instructions]
-    end
-    it "sends send_line to itself" do
       assert_send [@sender, :send_line, BufferFixture.instructions[0..1]]
     end
   end
