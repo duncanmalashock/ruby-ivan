508cf3231f99e3a8f0fb92560abb59ca0773d15a
Duncan Malashock <duncan@emissary.io>
2015-06-22 13:00:56 -0400

    Minor restructuring of Glyph Test

--- test/test_glyph.rb
+++ test/test_glyph.rb
@@ -1,22 +1,23 @@
 require_relative "minitest_helper"
 
 describe Glyph do
-  before do
-    @cube = Glyph.new(CubeFixture.geometry)
-  end
+  describe "when initialized correctly" do
+    before do
+      @cube = Glyph.new(CubeFixture.geometry)
+    end
 
-  describe "when initialized" do
-    it "has the right number of instructions" do
-      assert_equal 24, @cube.instructions.length
-    end
-    it "has the correct first coordinate" do
-      assert_equal -1.0, @cube.instructions[0].x
-      assert_equal -1.0, @cube.instructions[0].y
-      assert_equal 1.0, @cube.instructions[0].z
-    end
-    it "plays the role of Spatial" do
-      # Test that Glyph plays the Spatial role later!
+    describe "after initialization" do
+      it "has the right number of instructions" do
+        assert_equal 24, @cube.instructions.length
+      end
+      it "has the correct first coordinate" do
+        assert_equal -1.0, @cube.instructions[0].x
+        assert_equal -1.0, @cube.instructions[0].y
+        assert_equal 1.0, @cube.instructions[0].z
+      end
+      it "plays the role of Spatial" do
+        # Test that Glyph plays the Spatial role later!
+      end
     end
   end
-
 end
