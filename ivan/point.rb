5e1e6da5e91b11aec6cde52c0a7631e952d6bd12
Duncan Malashock <duncan@emissary.io>
2015-06-22 02:13:34 -0400

    Point has explicit x y z attributes, and tests for 2d/3d

--- ivan/point.rb
+++ ivan/point.rb
@@ -1,6 +1,19 @@
 class Point
-  attr_accessor :coordinate
-  def initialize (coordinate)
-    @coordinate = coordinate
+  attr_accessor :x, :y, :z
+  def initialize (x, y, z = nil)
+    @x = x;
+    @y = y;
+    @z = z;
+  end
+
+  def three_d?
+    return !@z.nil?
+  end
+
+  def screen_safe?(boundary)
+    return false if (@x < boundary[:x_min] || @x > boundary[:x_max])
+    return false if (@y < boundary[:y_min] || @y > boundary[:y_max])
+    return false if self.three_d?
+    return true
   end
 end
