09fca94ec6fb38d4b966fb01f67245932e6b480a
Duncan Malashock <duncan@emissary.io>
2015-06-26 09:20:19 -0400

    Ivan module with #load_model method for loading geometry data into memory

--- ivan.rb
+++ ivan.rb
@@ -5,3 +5,12 @@ require_relative 'ivan/has_transforms'
 require_relative 'ivan/glyph'
 require_relative 'ivan/sender'
 require_relative 'ivan/teensyv_sender'
+
+module Ivan
+  Models = {}
+  def load_model(model_name)
+    Models[model_name] = eval( \
+      File.read("#{ File.dirname($0) }/models/#{ model_name }.txt"))
+  end
+  module_function :load_model
+end
