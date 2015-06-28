e4ebdf276557278ce55c3c1135fd64621c577125
Duncan Malashock <duncan@emissary.io>
2015-06-27 22:58:46 -0400

    Use yaml for storing & loading model geometry

--- ivan.rb
+++ ivan.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 require_relative 'ivan/point'
 require_relative 'ivan/point_2d'
 require_relative 'ivan/point_3d'
@@ -7,10 +9,34 @@ require_relative 'ivan/sender'
 require_relative 'ivan/teensyv_sender'
 
 module Ivan
+  @model_path = nil
   Models = {}
-  def load_model(model_name)
-    Models[model_name] = eval( \
-      File.read("#{ File.dirname($0) }/models/#{ model_name }.txt"))
+  
+  def set_model_path(path)
+    @model_path = path
   end
-  module_function :load_model
+
+  def load_model(model_name)
+    model_file = File.read("#{ @model_path }/#{ model_name }.yml")
+    if model_file then
+      Models[model_name] = YAML.load(model_file)
+    end
+  end
+
+  def copy_model(source, destination)
+    if Models[source] then
+      Models[destination] = Models[source]
+    end
+  end
+
+  def save_model(model_name)
+    yaml_out = YAML.dump(Models[model_name])
+    if yaml_out then
+      file_out = File.open("#{ @model_path }/#{model_name}.yml","w")
+      file_out << yaml_out
+      file_out.close
+    end
+  end
+  
+  module_function :set_model_path, :load_model, :copy_model, :save_model
 end
