d718def43c3c355708eb334cd6d3f3c3bc789f40
Duncan Malashock <duncanmalashock@gmail.com>
2015-09-07 17:05:00 -0400

    implement TeensyVConnection, an implementation detail of Display

---
+++ lib/teensy_v_connection.rb
@@ -0,0 +1,18 @@
+# TeensyVConnection
+# Adapter from Display to teensyv hardware
+
+require 'serialport'
+
+module Ivan
+  class TeensyVConnection
+    attr_reader :serial_port
+
+    def initialize(device_path:, serial_port_class: SerialPort)
+      @serial_port = serial_port_class.new(device_path, 9600, 8, 1)
+    end
+
+    def draw(instructions)
+      return instructions.nil? ? nil : instructions.length
+    end
+  end
+end
---
+++ spec/ivan/teensy_v_connection_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+module Ivan
+  describe TeensyVConnection do
+    let(:teensyv) { TeensyVConnection.new(device_path: 'valid_path',
+      serial_port_class: SerialPortMock) }
+    describe "#initialize" do
+      it "assigns a :serial_port attr" do
+        expect(teensyv.serial_port).not_to be_nil
+        expect(teensyv.serial_port.class).to be(SerialPortMock)
+      end
+    end
+
+    describe "#draw" do
+      context "when called with nil" do
+        it "returns nil" do
+          expect(teensyv.draw(nil)).to eq(nil)
+        end
+      end
+      context "when called with instructions" do
+        it "returns the number of instructions drawn" do
+          expect(teensyv.draw([1,2,3])).to eq(3)
+        end
+      end
+    end
+  end
+end
