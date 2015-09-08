9e8b32482d58878ff1a251ffbf495ef6b60b8250
Duncan Malashock <duncanmalashock@gmail.com>
2015-09-08 12:17:07 -0400

    scale attr accepts a scalar or tuple

--- lib/glyph.rb
+++ lib/glyph.rb
@@ -3,11 +3,16 @@
 
 module Ivan
   class Glyph
-    attr_reader :geometry, :rotation
+    attr_reader :geometry, :rotation, :scale
 
-    def initialize(geometry:, rotation:[0,0,0])
+    def initialize(geometry:, rotation:[0,0,0], scale: 1.0)
       @geometry = geometry
       @rotation = rotation
+      if (!scale.respond_to? :length)
+        @scale = [scale, scale, scale]
+      else
+        @scale = scale
+      end
     end
 
     def render
--- spec/ivan/glyph_spec.rb
+++ spec/ivan/glyph_spec.rb
@@ -3,28 +3,49 @@ require 'spec_helper'
 module Ivan
   describe Glyph do
     context 'when initialized with only geometry' do
-      let(:cube) { Glyph.new(geometry: [1,2,3,4]) }
+      let(:cube) { Glyph.new(geometry: [1, 2, 3, 4]) }
       describe '#initialize' do
         it 'sets geometry attr correctly' do
-          expect(cube.geometry).to eq([1,2,3,4])
+          expect(cube.geometry).to eq([1, 2, 3, 4])
         end
         it 'has a default rotation value' do
-          expect(cube.rotation).to eq([0,0,0])
+          expect(cube.rotation).to eq([0, 0, 0])
         end
       end
       describe '#render' do
         it 'returns data for drawing' do
-          expect(cube.render).to eq([1,2,3,4])
+          expect(cube.render).to eq([1, 2, 3, 4])
         end
       end
     end
     context 'when initialized with a rotation value' do
       let(:cube) { Glyph.new(
-        geometry: [1,2,3,4],
-        rotation: [10,20,30]) }
+        geometry: [1, 2, 3, 4], 
+        rotation: [10, 20, 30]) }
       describe '#initialize' do
         it 'sets rotation attr correctly' do
-        expect(cube.rotation).to eq([10,20,30])
+          expect(cube.rotation).to eq([10, 20, 30])
+        end
+      end
+    end
+    context 'when initialized with a single scale value' do
+      let(:cube) { Glyph.new(
+        geometry: [1, 2, 3, 4], 
+        scale: 10) }
+      describe '#initialize' do
+        it 'sets scale attr correctly' do
+          expect(cube.scale).to eq([10, 10, 10])
+        end
+      end
+    end
+    context 'when initialized with a tuple scale value' do
+      let(:cube) { Glyph.new(
+        geometry: [1, 2, 3, 4], 
+        scale: [10, 10, 10]) }
+      describe '#initialize' do
+        it 'sets scale attr correctly' do
+          expect(cube.scale).to eq([10, 10, 10])
+        end
       end
     end
   end
