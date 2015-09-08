8febce4bfb66f873785309228fb2897ffa52d8db
Duncan Malashock <duncanmalashock@gmail.com>
2015-09-08 12:17:26 -0400

    notes to self on next steps

--- examples/app_1.rb
+++ examples/app_1.rb
@@ -1,14 +1,21 @@
 require_relative '../spec/mocks'
 require_relative '../lib/ivan'
 
-display = Ivan::Display.new(device_path: '/dev/tty.usbmodem54121',
+display = Ivan::Display.new(device_path: '/dev/tty.usbmodem54121', 
   connection_type: ConnectionTypeMock)
 output = Ivan::Output.new(display: display)
 
 cube_1 = Ivan::Glyph.new(
-  geometry: [1,2,3,4,5,6],
-  rotation: [0,10,0])
+  geometry: [1, 2, 3, 4, 5, 6],
+  rotation: [0, 10, 0])
 
 output.clear
 output.render cube_1
 puts output.send_to_display
+
+# TO DO:
+# Implement a Glyph
+# - with scale, rotation, position
+# Implement a Geometry
+# Implement a Point
+# Implement a Camera
