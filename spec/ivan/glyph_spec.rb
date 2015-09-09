626239e6bf7d18ae0fa4bfb4e2afc569b3569ad1
Duncan Malashock <duncanmalashock@gmail.com>
2015-09-08 22:39:13 -0500

    remove unnecessary mock objects

--- spec/ivan/glyph_spec.rb
+++ spec/ivan/glyph_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 module Ivan
   describe Glyph do
-    let (:stub_point) { PointMock.new }
-    let (:stub_geom) { GeometryMock.new }
+    let (:stub_geom) { double('geometry_mock') }
     context 'when initialized with only geometry' do
       let(:cube) { Glyph.new(geometry: stub_geom) }
       describe '#initialize' do
@@ -18,10 +17,9 @@ module Ivan
         end
       end
       describe '#render' do
-        it 'returns rendered points equal to its geometry attr' do
-          expect(cube.render[0].x).to eq(stub_geom.points[0].x)
-          expect(cube.render[0].y).to eq(stub_geom.points[0].y)
-          expect(cube.render[0].z).to eq(stub_geom.points[0].z)
+        it 'calls #render on its geometry attr' do
+          expect(cube.geometry).to receive(:render)
+          cube.render
         end
       end
     end
@@ -32,12 +30,6 @@ module Ivan
       describe '#initialize' do
         it 'sets position attr correctly' do
           expect(cube.position).to eq([30, 20, 10])
-        end
-      end
-      describe '#render' do
-        it 'transforms the points to the position' do
-          expect(cube.geometry).to receive(:scale)
-          cube.render
         end
       end
     end
