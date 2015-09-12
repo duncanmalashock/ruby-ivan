cc62c5666eadfa7361154b9421899697cf083edd
Duncan Malashock <duncanmalashock@gmail.com>
2015-09-12 01:16:12 -0400

    Remember to define Point inside Ivan namespace

--- lib/point.rb
+++ lib/point.rb
@@ -1,9 +1,11 @@
-class Point
+module Ivan
   # A single point in 3D space
-  attr_reader :x, :y, :z
-  def initialize(x: 0, y: 0, z: 0)
-    @x = x
-    @y = y
-    @z = z
+  class Point
+    attr_reader :x, :y, :z
+    def initialize(x: 0, y: 0, z: 0)
+      @x = x
+      @y = y
+      @z = z
+    end
   end
 end
