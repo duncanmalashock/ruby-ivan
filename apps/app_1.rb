4891b6f9c8ed6565c6cb17d99fc446248f747e23
Duncan Malashock <duncanmalashock@gmail.com>
2015-09-16 16:25:08 -0400

    revise test app to use new features

--- apps/app_1.rb
+++ apps/app_1.rb
@@ -1,19 +1,10 @@
-require_relative '../spec/mocks'
 require_relative '../lib/ivan'
 
 display = Ivan::Display.new(
-  device_path: '/dev/tty.usbmodem54121',
-  connection_type: ConnectionTypeMock)
+  device_path: '/dev/tty.usbmodem54121')
 output = Ivan::Output.new(display: display)
 
 cube_1 = Ivan::Glyph.new(
-  model: Ivan::Model.new([
-    Ivan::Point.new(x: 2, y: 5, z: 0.3)
-  ]),
-  position: [2, 2, 2],
-  scale: 2.0,
-  rotation: [0, 10, 0])
+  model: Ivan::Model.new_from_file("apps/models/cube.yml"))
 
-output.clear
-output.render cube_1
-puts output.send_to_display
+output.render_and_send cube_1
