fee14d7596d3dbd76c9ecb2dc49b33a83232fe30
Duncan Malashock <duncanmalashock@gmail.com>
2015-09-16 22:29:58 -0400

    little changes to test app

--- apps/app_1.rb
+++ apps/app_1.rb
@@ -5,6 +5,13 @@ display = Ivan::Display.new(
 output = Ivan::Output.new(display: display)
 
 cube_1 = Ivan::Glyph.new(
-  model: Ivan::Model.new_from_file('apps/models/cube.yml'))
+  model: Ivan::Model.new_from_file('apps/models/cube.yml'),
+  scale: 0.25)
 
-output.render_and_send cube_1
+x = 0
+while (true) do
+  x += 0.01
+  cube_1.scale = Math.sin(x) * 0.4 + 0.5
+  output.render_and_send cube_1
+  sleep 0.01
+end
