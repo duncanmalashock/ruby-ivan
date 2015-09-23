dcd9918ec5bc93bec2bc00f3c530eda6a461d431
Duncan Malashock <duncanmalashock@gmail.com>
2015-09-22 23:19:56 -0400

    Display implements input_boundary and output_boundary

--- lib/display.rb
+++ lib/display.rb
@@ -1,11 +1,25 @@
 module Ivan
   # Creates a connection to a display device to which instructions can be sent
   class Display
-    attr_reader :connection
+    attr_reader :connection, :input_boundary, :output_boundary
 
-    def initialize(device_path:, connection_type: DeviceConnection)
+    def initialize(device_path:, connection_type: DeviceConnection,
+          input_boundary: {
+            x_min: -100.0,
+            y_min: -100.0,
+            x_max: 100.0,
+            y_max: 100.0
+          },
+          output_boundary: {
+            x_min: 0,
+            y_min: 0,
+            x_max: 255,
+            y_max: 255
+          })
       if valid_device_path?(device_path)
         @connection = connection_type.new(device_path: device_path)
+        @input_boundary = input_boundary
+        @output_boundary = output_boundary
       else
         return nil
       end
--- spec/ivan/display_spec.rb
+++ spec/ivan/display_spec.rb
@@ -3,29 +3,31 @@ require 'spec_helper'
 module Ivan
   describe Display do
 
-    it 'has a boundary'
+    let(:display) { Display.new(device_path: 'valid_path',
+      connection_type: ConnectionMock) }
+
+    it 'has input and output boundaries' do
+      expect(display.input_boundary).not_to be(nil)
+      expect(display.output_boundary).not_to be(nil)
+    end
 
     describe '#initialize' do
       context 'when given a valid device path' do
-        let(:display) { Display.new(device_path: 'valid_path',
-          connection_type: ConnectionMock) }
         it 'creates a connection to a device' do
           expect(display.connection).to be_truthy
         end
       end
 
       context 'when given an invalid device path' do
-        let(:display) { Display.new(device_path: '',
+        let(:bad_display) { Display.new(device_path: '',
           connection_type: ConnectionMock) }
         it 'has a nil connection attribute' do
-          expect(display.connection).to be nil
+          expect(bad_display.connection).to be nil
         end
       end
     end
 
     describe '#send_instructions' do
-      let(:display) { Display.new(device_path: 'valid_path',
-        connection_type: ConnectionMock) }
       it 'sends instructions to the device' do
         expect(display.connection).to receive(:draw)
         result = display.send_instructions(['some instructions!'])
@@ -34,8 +36,8 @@ module Ivan
         result = display.send_instructions(['some instructions!'])
         expect(result).to be(1)
       end
-      it 'adjusts scale of points drawn to fit its boundary'
-      it 'clips lines outside its boundary'
+      it 'adjusts scale of points drawn to fit its output boundary'
+      it 'clips lines outside its input boundary'
     end
   end
 end
